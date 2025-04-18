8c91e1a5d23b24852b4a20c09edf3e451e0cc502
cjchoejor <ngawangchoejorw@gmail.com>
2025-04-18 06:24:15 +0600

    new with chatgpt

---
+++ config.js
@@ -0,0 +1,2 @@
+// OpenAI API key - keep this file in .gitignore
+export const OPENAI_API_KEY = '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';
